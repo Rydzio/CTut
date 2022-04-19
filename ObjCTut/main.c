5a8ae3285c77ad0e930b3a79869d4164a6c9e6b3
rytych <michal.rytych@geminibs.com>
2022-04-19 16:09:56 +0200

    If, else, logic operators

--- ObjCTut/main.c
+++ ObjCTut/main.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
 #include <math.h>
 #include <float.h>
+#include <stdbool.h>
 
 int main(int argc, const char * argv[]) {
     
-    float pi = 3.1415926;
+    int age = 13;
     
-    printf("3.1415926 + .0000001 = %.7f\n", pi + .0000001);
+    if (age <= 6) {
+        printf("You are in kindergarten\n");
+    } else if (age <= 13) {
+        printf("You're in elementary\n");
+    } else {
+        printf("You're in highschool\n");
+    }
     
-    printf("Precise Decimal Digits : %d\n", FLT_DIG);
+    if (age >= 12 || age <= 13) {
+        printf("You're in elementary");
+    }
+    bool isElementary = ((age >= 12) && (age <= 13));
+    
+    printf("Is in elementery: %d\n", isElementary);
+    
+    printf("Opposite of True: %d\n", (!true));
+    
+    bool isHighschool = (age > 13) ? 1 : 0;
+    
+    printf("Is in HighSchool : %d\n", isHighschool);
     
     return 0;
 }
