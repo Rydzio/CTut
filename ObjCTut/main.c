84b124365d503f5a9eaa19c63c1f19db465a68b9
rytych <michal.rytych@geminibs.com>
2022-04-19 14:29:50 +0200

    Initial Commit

---
+++ ObjCTut/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ObjCTut
+//
+//  Created by Michał Rytych on 19/04/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
