8d5d735292881dd7e6c42162eb737d3dbce2f74b
rytych <michal.rytych@geminibs.com>
2022-04-19 14:59:43 +0200

    For loop

--- ObjCTut/main.c
+++ ObjCTut/main.c
@@ -1,14 +1,9 @@
-//
-//  main.c
-//  ObjCTut
-//
-//  Created by Michał Rytych on 19/04/2022.
-//
-
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+//    *argv = "I am happy";
+    for (int i = 0; i < argc; i++) {
+        printf("arg %d : %s\n", i, argv[i]);
+    }
     return 0;
 }
